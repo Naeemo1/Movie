4598ba5cb46c17d756f3524fa7cec449507af2a1
naiemhisham@gmail.com <naiemhisham@gmail.com>
2020-11-21 22:53:02 +0200

    Java Script @ref

---
+++ Movies/Movies/Client/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Client.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime js, string meesage)
+        {
+            return await js.InvokeAsync<bool>("confirm", meesage);
+        }
+
+        public static async ValueTask Print(this IJSRuntime js, string message)
+        {
+             await js.InvokeVoidAsync("my_function", message);
+        }
+
+    }
+}
